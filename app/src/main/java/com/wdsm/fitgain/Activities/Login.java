343007f64542b8cb9a3ccb5fcc7447b4ca66e1ab
theXarxes <mccube00@gmail.com>
2021-12-28 19:31:14 +0100

    Log + Register + Home
    Home ma template toolbara do wklejenia we wszystke klasy oprócz login i register

--- app/src/main/java/com/wdsm/fitgain/Activities/Login.java
+++ app/src/main/java/com/wdsm/fitgain/Activities/Login.java
@@ -28,7 +28,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
         firebaseAuth = FirebaseAuth.getInstance();
 
         etLog = (EditText) findViewById(R.id.etEmail);
--- app/src/main/java/com/wdsm/fitgain/Entities/User.java
+++ app/src/main/java/com/wdsm/fitgain/Entities/User.java
@@ -1,5 +1,31 @@
 package com.wdsm.fitgain.Entities;
 
-public class User {
 
+public class User {
+    private static User instance;
+    private String eMail;
+    private String login;
+
+    public static User getInstance() {
+        if (instance == null){
+            instance = new User();
+        }
+        return instance;
+    }
+
+    public String getEMail() {
+        return eMail;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setEMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }

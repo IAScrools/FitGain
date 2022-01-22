23017fb8501deadc637b2cc1c7f137a04d485578
Hubert Krzeminski <darkexone@gmail.com>
2022-01-22 03:25:16 +0100

    Implement user step count retrieving from Google Fit API

--- app/src/main/java/com/wdsm/fitgain/Activities/Home.java
+++ app/src/main/java/com/wdsm/fitgain/Activities/Home.java
@@ -1,15 +1,24 @@
 package com.wdsm.fitgain.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.wdsm.fitgain.R;
+import com.wdsm.fitgain.Utils.FitnessDataUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Home extends AppCompatActivity {
 
@@ -17,6 +26,10 @@ public class Home extends AppCompatActivity {
     private TextView logOut;
     private FirebaseAuth firebaseAuth;
 
+    FitnessOptions fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE, FitnessOptions.ACCESS_READ)
+            .build();
+    String dataType = Manifest.permission.ACTIVITY_RECOGNITION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,32 @@ public class Home extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         bBack = (Button) findViewById(R.id.bBack);
         logOut = (TextView) findViewById(R.id.tvLogOut);
+
+        if (FitnessDataUtils.checkAndroidPermissions(this, dataType)) {
+            FitnessDataUtils.requestAndroidPermissions(this,dataType);
+        }
+
+        if (!FitnessDataUtils.checkGoogleFitPermissions(this, fitnessOptions)) {
+            FitnessDataUtils.requestGoogleFitPermissions(this, this, fitnessOptions);
+        }
+
+        Timestamp startDate = new Timestamp(new Date());
+        Timestamp endDate = new Timestamp(new Date());
+
+        // set startDate and endDate values for testing purposes
+        // replace with data from Firebase
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy hh:mm:ss");
+        String startDateString = "21-01-2022 21:16:00";
+        String endDateString = "21-01-2022 22:35:00";
+
+        try {
+            startDate = new Timestamp(sdf.parse(startDateString));
+            endDate = new Timestamp(sdf.parse(endDateString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(FitnessDataUtils.getActivityValueFromRange(this, startDate, endDate, DataType.TYPE_STEP_COUNT_DELTA));
 
         bBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +80,4 @@ public class Home extends AppCompatActivity {
             }
         });
     }
-
-
 }
